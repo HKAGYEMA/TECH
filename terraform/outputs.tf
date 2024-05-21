d237b9859dfb1e38a7259b7f9d35bd6ded2dbf58
Harris Agyeman <harrisagyeman@hotmail.com>
2024-05-21 15:39:30 +0100

    added

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,17 @@
-output "vpc-out" {
-  value = module.vpc
+# output "vpc-out" {
+#   value = module.vpc
+# }
+
+
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "public_subnets" {
+  value = module.vpc.public_subnets
+}
+
+output "private_subnets" {
+  value = module.vpc.private_subnets
 }
